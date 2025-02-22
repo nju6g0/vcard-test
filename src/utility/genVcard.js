f0d0c7a5820e850f8c56e7feba2cc8a97b82a346
YiShan Su <nju6g0@gmail.com>
2025-02-22 14:03:36 +0800

    handle vcard

--- src/utility/genVcard.js
+++ src/utility/genVcard.js
@@ -1,8 +1,11 @@
 const makeVCardVersion = () => `VERSION:3.0`
+const makeVCardInfo = (info) => `N:${info}`
 const makeVCardName = (name) => `FN:${name}`
 const makeVCardOrg = (org) => `ORG:${org}`
 const makeVCardTitle = (title) => `TITLE:${title}`
+const makeVCardPhoto = (img) => `PHOTO;TYPE=JPEG;ENCODING=b:[${img}]`
 const makeVCardTel = (phone) => `TEL;TYPE=WORK,VOICE:${phone}`
+const makeVCardAdr = (address) => `ADR;TYPE=WORK,PREF:;;${address}`
 const makeVCardEmail = (email) => `EMAIL:${email}`
 const makeVCardTimeStamp = () => `REV:${new Date().toISOString()}`
 
@@ -10,10 +13,13 @@ const makeVCardTimeStamp = () => `REV:${new Date().toISOString()}`
 export const genVcard = (values) => {
   const vcard = `BEGIN:VCARD
     ${makeVCardVersion()}
+    ${makeVCardInfo('cardInfo')}
     ${makeVCardName(values.name)}
     ${makeVCardOrg(values.company)}
     ${makeVCardTitle(values.title)}
+    ${makeVCardPhoto('https://unsplash.com/photos/an-elephant-is-walking-down-a-dirt-road-I6Qh6UAYazg')}
     ${makeVCardTel(values.phone)}
+    ${makeVCardAdr('addressEl')}
     ${makeVCardEmail(values.email)}
     ${makeVCardTimeStamp()}
     END:VCARD`
