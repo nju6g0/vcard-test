b8d77cce4d7c20d789a3c8d110558412a313bd8f
YiShan Su <nju6g0@gmail.com>
2025-02-22 20:50:53 +0800

    test

--- src/utility/genVcard.js
+++ src/utility/genVcard.js
@@ -1,3 +1,42 @@
+import VCard from 'vcard-creator'
+
+function genVcardFromCreator(values) {
+  // Define a new vCard
+  const myVCard = new VCard()
+
+  // Some variables
+  const lastname = 'Desloovere'
+  const firstname = 'Jeroen'
+  const additional = ''
+  const prefix = ''
+  const suffix = ''
+
+  myVCard
+    // Add personal data
+    .addName(lastname, firstname, additional, prefix, suffix)
+    // Add work data
+    .addCompany('Siesqo')
+    .addJobtitle('Web Developer')
+    .addRole('Data Protection Officer')
+    .addEmail('info@jeroendesloovere.be')
+    .addPhoneNumber(1234121212, 'PREF;WORK')
+    .addPhoneNumber(123456789, 'WORK')
+    .addAddress(
+      null,
+      null,
+      'street',
+      'worktown',
+      null,
+      'workpostcode',
+      'Belgium'
+    )
+    .addSocial('https://twitter.com/desloovere_j', 'Twitter', 'desloovere_j')
+    .addURL('http://www.jeroendesloovere.be')
+
+  console.log(myVCard.toString())
+  return myVCard
+}
+
 const makeVCardVersion = () => `VERSION:3.0`
 const makeVCardInfo = (info) => `N:${info}`
 const makeVCardName = (name) => `FN:${name}`
@@ -10,7 +49,7 @@ const makeVCardEmail = (email) => `EMAIL:${email}`
 const makeVCardTimeStamp = () => `REV:${new Date().toISOString()}`
 
 // ref: https://dev.to/walternascimentobarroso/make-vcard-with-js-2afl
-export const genVcard = (values) => {
+const genVcard = (values) => {
   const vcard = `BEGIN:VCARD
     ${makeVCardVersion()}
     ${makeVCardInfo('cardInfo')}
@@ -25,3 +64,5 @@ export const genVcard = (values) => {
     END:VCARD`
   return vcard
 }
+
+export { genVcardFromCreator, genVcard }
